043ba2ef7f200ebf2cd357b00d1dec64caaf0994
userkw <elkis.kawthar11@gmail.com>
2024-01-09 02:32:38 -0800

    done

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    try:
-        result = my_list[idx]
-    except IndexError:
-        result = None
-    return result
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
