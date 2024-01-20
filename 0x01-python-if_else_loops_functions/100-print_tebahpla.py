26044ddf36cf70134bfbca06873c06bc55887dcd
userkw <elkis.kawthar11@gmail.com>
2024-01-20 15:04:27 -0800

    done

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    if i % 2 == 0:
-        diff = 0
-    else:
-        diff = 32
-    print('{}'.format(chr(i - diff)), end='')
+for c in range(ord('z'), ord('a') - 1, -2):
+    print("{:c}{:s}".format(c, chr(c - 33)), end="")
