83edd4c1249a4908a712f9327c593cffc50b2fed
userkw <elkis.kawthar11@gmail.com>
2024-01-22 09:45:42 -0800

    done

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+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
+
+
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    c = 0
+    i = 0
+    try:
+        while c < x:
+            if type(my_list[i]) == int:
+                print("{:d}".format(my_list[i]), end="")
+                c += 1
+            i += 1
+        print()
+    except IndexError:
+        print()
+    return c
