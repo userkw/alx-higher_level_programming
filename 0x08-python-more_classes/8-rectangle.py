cb0692985873eef3fd506ca07e31dc9b7086ecbb
userkw <elkis.kawthar11@gmail.com>
2024-02-24 06:04:41 -0800

    done

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""Module 8-rectangle
-Defines a Rectangle class.
-"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
     """Rectangle class defined by width and height.
     Attributes:
-        number_of_instances: number of Rectangle instances,
-        increments with every instantitation,
-        decrements with every deletion
+        number_of_instances: number of Rectangle instances
     """
 
     number_of_instances = 0
@@ -26,8 +22,7 @@ class Rectangle:
         Rectangle.number_of_instances += 1
 
     def __str__(self):
-        """Returns an informal and nicely printable string representation
-        of a Rectangle instance, filled with the '#' character."""
+        """Returns an informal and nicely printable string representation."""
         if self.__height == 0 or self.__width == 0:
             return ''
         rec_str = ''
@@ -38,9 +33,7 @@ class Rectangle:
         return rec_str[:-1]
 
     def __repr__(self):
-        """Return a string representation of a Rectangle instance
-        that is able to recreate a new instance by using eval()
-        """
+        """Return a string representation of a Rect"""
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
@@ -50,7 +43,7 @@ class Rectangle:
 
     @property
     def width(self):
-        """Retrieves the width of a Rectangle instance."""
+        """Retrieves the width of a Rect."""
         return self.__width
 
     @width.setter
@@ -67,7 +60,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """Retrieves the height of a Rectangle instance."""
+        """the height of a Rect."""
         return self.__height
 
     @height.setter
@@ -83,17 +76,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Calculates the area of a Rectangle instance
-        Returns:
-            Area of the the rectangle, given by height * width
-        """
+        """Calculates the area"""
         return self.__width * self.__height
 
     def perimeter(self):
-        """Calculates the perimeter of a Rectangle instance
-        Returns:
-            Perimeter of the rectangle, given by 2 * (height + width)
-        """
+        """Calculates the perimeter of a Rect"""
         if self.__height == 0 or self.__width == 0:
             return 0
         return 2 * (self.__width + self.__height)
